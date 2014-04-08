d44b97032fb7318acc69941c83652e6a9c0cbc0b
mdxbot <mdxbook@gmail.com>
2014-04-08 10:48:41 +0800

    0.1

--- WindowsApplication1/Form1.cs
+++ WindowsApplication1/Form1.cs
@@ -17,14 +17,14 @@ namespace WindowsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            char[,] nfa = new char[100,100];
+            char[,] nfa = new char[100, 100];
             char[] str;
             int[] s = new int[10];
             int[] f = new int[10];
-            int count=2;//总结点数
+            int count=1;//总结点数
             int flag=0;//记录未完成或
             int n = 0;//未完成左括号个数
-            int m=0;//起点终点计数
+            int m=0;//层级
             s[0] = 0;
             f[0] = 1;
             nfa[s[0],f[0]] = 'ε';
@@ -36,14 +36,15 @@ namespace WindowsApplication1
                 if ((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z') ||
                     (str[j] >= '0' && str[j] <= '9'))
                 {
-                    nfa[f[m],f[m] + 1] = str[j];
+                    nfa[f[m], f[m] + 1] = str[j];
                     count = count + 1;
                     f[m] = count; 
                     if (j + 1 == num)
                     {
                         if (flag == 1)
                         {
-                            nfa[f[m],f[m - 1]] = 'ε';
+                            nfa[f[m], f[m - 1]] = 'ε';
+                            m = m - 1;
                         }
                     }
                 }
@@ -51,10 +52,10 @@ namespace WindowsApplication1
                 {
                     if(flag == 1)
                     {
-                        nfa[f[m],f[m - 1]] = 'ε';
+                        nfa[f[m], f[m - 1]] = 'ε';
                         count = count + 1;
                         s[m] = count;
-                        nfa[s[m - 1],s[m]] = 'ε';
+                        nfa[s[m - 1], s[m]] = 'ε';
                     }
                     else if(flag == 0)
                     {
@@ -63,26 +64,64 @@ namespace WindowsApplication1
                         f[m] = f[m - 1];
                         count = count + 1;
                         f[m - 1] = count;
-                        nfa[f[m],f[m - 1]] = 'ε';
+                        nfa[f[m], f[m - 1]] = 'ε';
                         count = count + 1;
                         s[m] = count;
-                        nfa[s[m - 1],s[m]] = 'ε';
+                        nfa[s[m - 1], s[m]] = 'ε';
+                        f[m] = s[m];
                     }
                 }
                 else if (str[j] == '*')
                 {
-                    
+                    if (str[j - 1] != ')' && str[j - 1] != '*')
+                    {
+                        nfa[f[m] - 1, f[m]] = 'ε';
+                        nfa[f[m], f[m] + 1] = str[j - 1];
+                        count = count + 1;
+                        f[m] = count;
+                        m = m + 1;
+                        s[m] = f[m - 1] - 2;
+                        f[m] = f[m - 1];
+                    }
+                    count = count + 1;
+                    nfa[f[m], s[m] + 1] = 'ε';
+                    f[m] = count;
+                    nfa[s[m], f[m]] = 'ε';
+                    nfa[f[m] - 1, f[m]] = 'ε';
+                    count = count + 1;
+                    f[m] = count;
+                    nfa[f[m] - 1, f[m]] = 'ε';
+                    if (str[j - 1] != ')' && str[j - 1] != '*')
+                    {
+                        m = m - 1;
+                    }
                 }
                 else if (str[j] == '(')
                 {
                     n = n + 1;
+                    m = m + 1;
+                    count=count+1;
+                    s[m] = f[m - 1];
+                    f[m] = s[m] + 1;
+                    nfa[f[m - 1], f[m]] = 'ε';
                 }
                 else if (str[j] == ')')
                 {
                     n = n - 1;
+                    if (flag == 1)
+                    {
+                        flag = flag - 1;
+                        nfa[f[m], f[m - 1]] = 'ε';
+                        m = m - 1;
+                    }
+                    f[m - 1] = count + 1;
+                    count = count + 1;
+                    nfa[f[m], f[m - 1]] = 'ε';
+                    f[m] = f[m - 1];
+                    m = m - 1;
                 }
             }
-            MessageBox.Show("" + count + " " + f[0] + "");
+            //MessageBox.Show("" + count + " " + f[0] + " " + nfa[3,4] + " " + nfa[6,7] + "");
         }
     }
 }
