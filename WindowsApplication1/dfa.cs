a0dedaaa6a5ad2552013a753f83b7aae7b1aabd1
mdxbot <mdxbook@gmail.com>
2014-04-16 20:13:25 +0800

    0.2.6

---
+++ WindowsApplication1/dfa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsApplication1
+{
+    class dfa
+    {
+        private char[,] list = new char[50, 50];
+        private List<int> final;
+        private List<char> ch;
+        private List<List<int>> status;
+
+        public void todfa(nfa nfa1, string text)
+        {
+            ch = nfa1.getch(text);
+            int n = ch.Count;
+            int unmark = 1;
+            //char[,] dfa = new char[50, 50];
+            if (status.Count == 0)
+            {
+                List<int> start = new List<int>();
+                start.Add(0);
+                nfa1.smove(start, 'ε');
+            }
+            else
+            {
+                int tag = 0;
+                while (unmark > 0)
+                {
+                    unmark--;
+                    for (int i = 0; i < n; i++)
+                    {
+                        int temp;
+                        int mem = status.Count;
+                        temp = nfa1.smove(status[tag], ch[i]);
+                        if (temp == mem + 1)
+                        {
+                            unmark++;
+                        }
+                        list[tag, temp] = ch[i];
+                        tag = temp;
+                    }
+                }
+            }
+            int t = status.Count;
+            for (int s = 0; s < t; s++)
+            {
+                if (status[s].Contains(nfa1.f0))
+                {
+                    final.Add(s);
+                }
+            }
+        }
+    }
+}
--- WindowsApplication1/nfa.cs
+++ WindowsApplication1/nfa.cs
@@ -6,45 +6,123 @@ namespace WindowsApplication1
 {
     class nfa
     {
-        private char[,] list;
-        private int f0;
+        private char[,] list = new char[100, 100];
+        public int f0;
         private int count;
-        private List<char> ch;
         private List<List<int>> status;
-        public char[,] todfa()
+        
+        public void tonfa(string text)
         {
-            int n = ch.Count;
-            int unmark = 1;
-            char[,] dfa = new char[50,50];
-            if (status.Count == 0)
+            char[] str;
+            int[] s = new int[10];
+            int[] f = new int[10];
+            count = 1;//总结点数
+            int flag = 0;//记录未完成或
+            int n = 0;//未完成左括号个数
+            int m = 0;//层级
+            s[0] = 0;
+            f[0] = 1;
+            list[s[0], f[0]] = 'ε';
+            int num = text.Length;
+            str = text.ToCharArray();
+            for (int j = 0; j < num; j++)
             {
-                List<int> start = new List<int>();
-                start.Add(0);
-                smove(start, 'ε');
-            }
-            else
-            {
-                int tag = 0;
-                while (unmark > 0)
+                //判断字符
+                if ((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z') ||
+                    (str[j] >= '0' && str[j] <= '9'))
                 {
-                    unmark--;
-                    for (int i = 0; i < n; i++)
+                    list[f[m], f[m] + 1] = str[j];
+                    count = count + 1;
+                    f[m] = count;
+                    if (j + 1 == num)
                     {
-                        int temp;
-                        int mem = status.Count;
-                        temp = smove(status[tag], ch[i]);
-                        if (temp == mem+1)
+                        if (flag == 1)
                         {
-                            unmark++;
+                            list[f[m], f[m - 1]] = 'ε';
+                            m = m - 1;
                         }
-                        dfa[tag, temp] = ch[i];
-                        tag = temp;
+                    }
+                }
+                else if (str[j] == '|')
+                {
+                    if (flag == 1)
+                    {
+                        list[f[m], f[m - 1]] = 'ε';
+                        count = count + 1;
+                        s[m] = count;
+                        list[s[m - 1], s[m]] = 'ε';
+                    }
+                    else if (flag == 0)
+                    {
+                        flag = 1;
+                        m = m + 1;
+                        f[m] = f[m - 1];
+                        count = count + 1;
+                        f[m - 1] = count;
+                        list[f[m], f[m - 1]] = 'ε';
+                        count = count + 1;
+                        s[m] = count;
+                        list[s[m - 1], s[m]] = 'ε';
+                        f[m] = s[m];
+                    }
+                }
+                else if (str[j] == '*')
+                {
+                    if (str[j - 1] != ')' && str[j - 1] != '*')
+                    {
+                        list[f[m] - 1, f[m]] = 'ε';
+                        list[f[m], f[m] + 1] = str[j - 1];
+                        count = count + 1;
+                        f[m] = count;
+                        m = m + 1;
+                        s[m] = f[m - 1] - 2;
+                        f[m] = f[m - 1];
+                    }
+                    count = count + 1;
+                    list[f[m], s[m] + 1] = 'ε';
+                    f[m] = count;
+                    list[s[m], f[m]] = 'ε';
+                    list[f[m] - 1, f[m]] = 'ε';
+                    count = count + 1;
+                    f[m] = count;
+                    list[f[m] - 1, f[m]] = 'ε';
+                    f[m - 1] = f[m];
+                    m = m - 1;
+                }
+                else if (str[j] == '(')
+                {
+                    f[m] = f[m] + 1;
+                    list[f[m] - 1, f[m]] = 'ε';
+                    count = count + 1;
+                    n = n + 1;
+                    m = m + 1;
+                    count = count + 1;
+                    s[m] = f[m - 1] - 1;
+                    f[m] = f[m - 1] + 1;
+                    list[f[m - 1], f[m]] = 'ε';
+                }
+                else if (str[j] == ')')
+                {
+                    n = n - 1;
+                    if (flag == 1)
+                    {
+                        flag = flag - 1;
+                        list[f[m], f[m - 1]] = 'ε';
+                        m = m - 1;
+                    }
+                    f[m - 1] = count + 1;
+                    count = count + 1;
+                    list[f[m], f[m - 1]] = 'ε';
+                    f[m] = f[m - 1];
+                    if (str[j + 1] != '*')
+                    {
+                        m = m - 1;
                     }
                 }
             }
-            return dfa;
+            f0 = f[0];
         }
-
+       
         public List<int> move(int i, char x)
         {
             List<int> t = new List<int>();
@@ -139,6 +217,27 @@ namespace WindowsApplication1
                 }
             }
             return num;
+        }
+
+        public List<char> getch(string text)
+        {
+            List<char> ch = new List<char>();
+            char[] str;
+            int num = text.Length;
+            str = text.ToCharArray();
+            for (int j = 0; j < num; j++)
+            {
+                //判断字符
+                if ((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z') ||
+                    (str[j] >= '0' && str[j] <= '9'))
+                {
+                    if (ch.Contains(str[j]) == false)
+                    {
+                        ch.Add(str[j]);
+                    }
+                }
+            }
+            return ch;
         }
     }
 }
