b6a5a24608222061e8b35e0f5b4f44932916d562
mdxbot <mdxbook@gmail.com>
2014-05-16 21:13:49 +0800

    0.7.1

--- WindowsApplication1/Parsing/parsing.cs
+++ WindowsApplication1/Parsing/parsing.cs
@@ -10,8 +10,8 @@ namespace WindowsApplication1
 
         public void optimize()//消除左递归和左因子
         {
-            //grammar.Add("<s>→<a>a|b");
-            //grammar.Add("<a>→<a>c|<s>d|ε");
+            //grammar.Add("<s>→i<c>t<s>|i<c>t<s>e<s>|a");
+            //grammar.Add("<c>→b");
 
             //grammar.Add("<e>→<e>+<t>|<t>");
             //grammar.Add("<t>→<t>*<f>|<f>");
@@ -80,6 +80,7 @@ namespace WindowsApplication1
             {
                 direct(grammar[i], i);
             }
+            picklf();
         }
 
         public void direct(string str, int i)//消除直接左递归，参数为串和位置
@@ -158,15 +159,18 @@ namespace WindowsApplication1
             }
         }
 
-        public void picklf()//提取左因子
+        public void picklf()//提取直接左因子
         {
+            char ch = 'Z';
             for (int i = 0; i < grammar.Count; i++)
             {
+                bool flag = false;
                 int maxlength = 0;
-                string head = "";
-                string temphead = "";
                 string[] exp = grammar[i].Split(new char[2] { '|', '→' });
+                string temphead = "";
+                string changed = exp[0] + "→";
                 List<int> current = new List<int>();
+                List<int> temp = new List<int>();
                 for (int j = 1; j < exp.Length; j++)
                 {
                     if (exp[j].Length > maxlength)
@@ -174,15 +178,116 @@ namespace WindowsApplication1
                         maxlength = exp[j].Length;
                     }
                 }
-                int mark = 1;
-                while (mark < exp.Length)
+                while (findlf(exp) != 404)
                 {
-                    for (int j = 1; j < exp[j].Length; j++)
+                    int x = findlf(exp);
+                    flag = true;
+                    for (int m = 1; m < exp.Length; m++)
                     {
-                        
+                        int f = 1;
+                        if (exp[x][0] == '<')
+                        {
+                            f = 2;
+                        }
+                        if (exp[m].Substring(0, f) == exp[x].Substring(0, f))
+                        {
+                            current.Add(m);
+                        }
+                    }
+                    int count = current.Count;
+                    for (int t = 0; t < maxlength; t++)
+                    {
+                        for (int j = 0; j < count; j++)
+                        {
+                            for (int k = j + 1; k < count; k++)
+                            {
+                                if (exp[current[j]].Length >= t + 1 && exp[current[k]].Length >= t + 1)
+                                {
+                                    if (current.Count > temp.Count + 1)
+                                    {
+                                        if (exp[current[k]][t] == exp[current[j]][t])
+                                        {
+                                            temphead = temphead + exp[current[k]][t].ToString();
+                                        }
+                                        else
+                                        {
+                                            temp.Add(current[k]);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    int l = temphead.Length;
+                    string remain = "<" + ch.ToString() + ">→";
+                    for (int j = 0; j < count; j++)
+                    {
+                        if (exp[current[j]].Substring(l) != "")
+                        {
+                            remain = remain + exp[current[j]].Substring(l) + "|";
+                        }
+                        if (j == 0)
+                        {
+                            exp[current[j]] = temphead + "<" + ch.ToString() + ">";
+                            ch--;
+                        }
+                        else
+                        {
+                            exp[current[j]] = "";
+                        }
+                    }
+                    remain = remain + "ε";
+                    grammar.Insert(i + 1, remain);
+                    for (int k = 1; k < exp.Length; k++)
+                    {
+                        if (exp[k] != "")
+                        {
+                            changed = changed + exp[k];
+                            if (k + 1 < exp.Length)
+                            {
+                                changed = changed + "|";
+                            }
+                        }
                     }
                 }
+                if (flag)
+                {
+                    grammar.Remove(grammar[i]);
+                    grammar.Insert(i, changed);
+                }
             }
+        }
+
+        public int findlf(string[] exp)
+        {
+            int flag = 404;
+            for (int i = 1; i < exp.Length; i++)
+            {
+                for (int j = i + 1; j < exp.Length; j++)
+                {
+                    if (exp[i] != "" && exp[j] != "")
+                    {
+                        int x = 1;
+                        if (exp[i][0] == '<')
+                        {
+                            x = 2;
+                        }
+                        if (exp[i].Length >= x && exp[j].Length >= x)
+                        {
+                            if (exp[j].Substring(0, x) == exp[i].Substring(0, x))
+                            {
+                                flag = i;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (flag != 404)
+                {
+                    break;
+                }
+            }
+            return flag;
         }
     }
 }
